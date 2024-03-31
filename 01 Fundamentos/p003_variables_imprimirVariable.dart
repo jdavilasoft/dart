93b30d796bed63a26e4efafad8e149e62fa3aa6e
Javier <jdavila.soft@gmail.com>
2024-03-31 15:53:28 -0500

    Imprimir datos

---
+++ 01 Fundamentos/p003_variables_imprimirVariable.dart
@@ -0,0 +1,10 @@
+
+// Formas de imprimir una varibale
+int miVariable = 6969;
+
+void main() {
+  print(miVariable);
+  print("Mi variable: $miVariable");
+  print("Mi variable: ${miVariable.toString()}");
+  print("Mi variable: " + miVariable.toString());
+}
